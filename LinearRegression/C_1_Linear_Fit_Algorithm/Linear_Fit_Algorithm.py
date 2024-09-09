2b8895d99307878535c7486bac2885f2a50e7c09
Akhil Singh Chauhan <akhilsin@buffalo.edu>
2024-09-08 23:14:59 -0400

    Updating Repo

--- LinearRegression/C_1_Linear_Fit_Algorithm/Linear_Fit_Algorithm.py
+++ LinearRegression/C_1_Linear_Fit_Algorithm/Linear_Fit_Algorithm.py
@@ -1,8 +1,9 @@
+#Linear fit Algorithm
 def linear_fit(inputs):
     """
     :type inputs: List[float]
     :rtype: Tuple[float, float]
-    """
+    """    
     n = len(inputs)
     sum_x = sum(inputs)
     sum_y = sum(range(n))
@@ -14,6 +15,36 @@ def linear_fit(inputs):
     intercept = (sum_y -slope*sum_x)/n
     
     return slope,intercept
+
+# Test cases
+print(linear_fit([10.5, 35.3, 19.0, 47.8, 25.0, 8.1]))
+print(linear_fit([5.0, 2.3, 7.8, 1.0, 4.4, 8.2, 0.6]))
+print(linear_fit([3.1415, 2.7182, 1.4142, 1.732, 0.5772, 2.6854]))
+
+
+
+
+#Accepted ans in this format
+
+from decimal import Decimal, getcontext
+def linear_fit(inputs):
+    """
+    :type inputs: List[float]
+    :rtype: Tuple[float, float]
+    """
+    getcontext().prec = 50  # Set precision to 50 decimal places
+    
+    n = len(inputs)
+    sum_x = sum(inputs)
+    sum_y = sum(range(n))
+    sum_x_squared = sum(x**2 for x in inputs)
+    sum_xy = sum(inputs[i]*i for i in range(n))
+    
+    slope = (n*sum_xy - sum_x*sum_y)/(n*sum_x_squared - sum_x**2)
+    
+    intercept = (sum_y -slope*sum_x)/n
+    
+    return float(slope), float(intercept)
 
 # Test cases
 print(linear_fit([10.5, 35.3, 19.0, 47.8, 25.0, 8.1]))
